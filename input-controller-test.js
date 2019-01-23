2387a126191a3d3c6b894534b6f53773998abdad
Eugene <Eugene030993@mail.ru>
2019-01-23 19:04:54 +0300

    Универсальный контроллей ввода, реализация конструктора и метода bindActions

--- input-controller-test.js
+++ input-controller-test.js
@@ -1,1 +1,28 @@
+let testTarget = document.getElementById('testDiv')
 
+let testObject = {
+  "left": {
+    keys: [37,65],
+    enabled: false
+  },
+  "right": {
+    keys: [39,68]
+  },
+}
+
+let testObject2 = {
+  "left": {
+    keys: [39,100],
+    enabled: false
+  },
+  "right": {
+    keys: [39,68],
+    enabled: false
+  },
+}
+
+let a = new InputController(testObject, testTarget);
+
+console.log(a.actionsToBind);
+a.bindActions = testObject2;
+console.log(a.actionsToBind);
--- input-controller.js
+++ input-controller.js
@@ -1,1 +1,55 @@
+'use strict'
 
+class InputController {
+  constructor (actions_to_bind, target) {
+    this.actionsToBind = actions_to_bind;
+    this.target = target;
+    this.enabled;
+    this.focused;
+  }
+  
+  static get ACTION_ACTIVATED() {
+    return "input-controller:action-activated";
+  }
+  static get DEACTION_ACTIVATED() {
+    return "input-controller:action-deactivated";
+  }
+
+  set bindActions(actions_to_bind) {
+    this.actionsToBind = actions_to_bind;
+  }
+
+  attach(target, dont_enable) {
+    this.target = true;
+    if(!dont_enable) {
+      //this.enableAction()
+      //this.target.addEventListener('keypress', );
+      //this.target.addEventListener('keypress', this.enableAction);
+    }
+  }
+
+  detach() {
+    this.target = false;
+    //this.disableAction()
+    //this.target.removeEventListener('keypress', );
+    //this.target.removeEventListener('keypress', this.disableAction);
+  }
+
+  enableAction(action_name) {
+    
+  }
+
+  disableAction(action_name) {
+    
+  }
+
+  isActionActive(action) {
+    if(this.actionsToBind[action].enabled) return false;
+    return true;
+  }
+
+  isKeyPressed(keyCode) {
+    
+  }
+
+}
