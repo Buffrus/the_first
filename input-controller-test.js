b08551417302b15b11f80113058715b3899df456
Eugene <Eugene030993@mail.ru>
2019-01-24 14:38:33 +0300

    Версия для тестирования

--- input-controller-test.js
+++ input-controller-test.js
@@ -1,28 +1,146 @@
-let testTarget = document.getElementById('testDiv')
 
 let testObject = {
   "left": {
-    keys: [37,65],
+    keys: [37],
     enabled: false
   },
   "right": {
-    keys: [39,68]
+    keys: [39]
   },
 }
 
 let testObject2 = {
   "left": {
-    keys: [39,100],
-    enabled: false
+    keys: [65],
+    enabled: true
   },
   "right": {
-    keys: [39,68],
+    keys: [68],
     enabled: false
   },
+  "jump": {
+    keys: [32]
+  }
 }
 
-let a = new InputController(testObject, testTarget);
+let testTarget = document.getElementById('testDiv');
+let box = document.getElementById('box');
 
-console.log(a.actionsToBind);
-a.bindActions = testObject2;
-console.log(a.actionsToBind);
+// let inputController = new InputController(testObject, testTarget);
+
+let inputController = new InputController();
+inputController.bindActions( testObject );
+
+inputController.attach( testTarget, !false );
+
+// inputController.attach( testTarget );
+// inputController.enabled = false;
+
+
+var jump;
+var jumpFinish;
+var stepJump
+
+setInterval(function(){
+  
+  var xs, ys;
+  if( inputController.isActionActive("left")){
+    xs = -1;
+  }else if( inputController.isActionActive("right")){
+    xs = 1;
+  }
+  // xs = Math.random() > .5 ? 1 : -1;
+  if( xs !== undefined ){
+    box.style.left = parseInt( box.style.left ) + xs + "%";
+  }
+
+  
+  if( inputController.isActionActive("jump")){
+    if(!jump) {
+      jump = true;
+      box.style.backgroundColor = "black";
+      stepJump = 3;
+      jumpFinish = false;
+    }
+  }
+
+  if(jump) {
+    console.log(stepJump);
+    box.style.top = (50 - stepJump) + "%";
+    if(jumpFinish) {
+      stepJump-= 3;
+    } else {
+      stepJump+= 3;
+      if(stepJump >= 20) {
+        jumpFinish = true;
+      }
+    }
+    if(stepJump <= -3) {
+      console.log(stepJump);
+      jump = false;
+      box.style.backgroundColor = "red";
+    }
+  }
+
+}, 40);
+
+
+// setInterval(function() {
+//   console.log(inputController.isKeyPressed(37));
+// }, 2000);
+
+
+
+// >>> BUTTON >>>
+function addInput( name, type, onClick , id){
+  var buttons = document.getElementsByClassName('buttons')[0];
+  // console.log('buttons:', buttons);
+  var button = document.createElement('input');
+  buttons.appendChild( button );
+  button.value = name;
+  button.type = type;
+  button.id = id;
+  button.onclick = onClick;
+}
+
+//
+addInput( 'Log inputController.actions', 'button', function(){
+  console.log( inputController.actions );
+});
+
+//
+addInput( 'bindActions', 'button',  function(){
+  inputController.bindActions( testObject2 );
+});
+
+//
+addInput( 'attach', 'button',  function(){
+  inputController.attach(testTarget, false);
+});
+
+//
+addInput( 'detach', 'button',  function(){
+  inputController.detach();
+});
+
+//
+addInput( 'action', 'text', undefined, 'actionField');
+
+//
+addInput( 'enableAction', 'button', function(){
+  let actionField = document.getElementById("actionField");
+  inputController.enableAction(actionField.value);
+});
+
+//
+addInput( 'disableAction', 'button', function(){
+  let actionField = document.getElementById("actionField");
+  inputController.disableAction(actionField.value);
+});
+
+
+
+// <<< BUTTON <<<
+// console.log(a.actionsToBind);
+// a.bindActions = testObject2;
+// console.log(a.actionsToBind);
--- input-controller.js
+++ input-controller.js
@@ -1,13 +1,8 @@
 'use strict'
 
 class InputController {
-  constructor (actions_to_bind, target) {
-    this.actionsToBind = actions_to_bind;
-    this.target = target;
-    this.enabled;
-    this.focused;
-  }
   
+  // CONSTANTS
   static get ACTION_ACTIVATED() {
     return "input-controller:action-activated";
   }
@@ -15,41 +10,138 @@ class InputController {
     return "input-controller:action-deactivated";
   }
 
-  set bindActions(actions_to_bind) {
-    this.actionsToBind = actions_to_bind;
+
+
+  constructor ( actions_to_bind, target ) {
+    
+    this.actions = {};
+
+    /*
+    BindedAction - class name
+    bindedAction - class instance, method name
+    binded_action - variable
+    _binded_action - private variable
+    BINDED_ACTION - constant
+    $binded_action - JQuery object -> $('.binded_action')
+    */
+
+    this.enabled;
+    this.focused;
+
+    /*
+    for (let action in actions_to_bind) {
+      if(!('enabled' in actions_to_bind[action]))
+      {
+        this.actions[action].enabled = true;
+      }
+    }
+    */
+
+    if( actions_to_bind ) this.bindActions( actions_to_bind );
+    if( target ) this.attach( target );
+  }
+  
+
+  bindActions( actions_to_bind ) {
+
+    for (let action_name in actions_to_bind) {
+
+      var action_to_bind = actions_to_bind[action_name];
+      var action = this.actions[action_name];
+
+      // Добавить отсутствующие активности
+      if(!action) {
+        action = this.actions[action_name] = {};
+        action.keys = [];
+        action.enabled = true;
+        action.active = false;
+      }
+
+      action.keys = [...action.keys, ...action_to_bind.keys];
+
+      if( action_to_bind.enabled !== undefined ) action.enabled = action_to_bind.enabled;
+
+    };
+
   }
 
   attach(target, dont_enable) {
-    this.target = true;
-    if(!dont_enable) {
-      //this.enableAction()
-      //this.target.addEventListener('keypress', );
-      //this.target.addEventListener('keypress', this.enableAction);
+    
+    this.detach();
+
+    this.target = target;
+    if( dont_enable !== undefined )  this.enabled = dont_enable;
+      
+    //this.enableAction()
+    //target.addEventListener('keypress', );
+    //target.addEventListener('keypress', this.enableAction);
+    if( !this.onKeyDown ){
+      
+      this.onKeyDown = (e) => {
+        console.log("onKeyDown: ", e.keyCode);
+        for (let action_name in this.actions) {
+          let action = this.actions[action_name];
+          if(action.keys.indexOf( e.keyCode ) != -1 && action.enabled) {
+            action.active = true;          
+          }
+        }
+        // }
+      }
+
+      this.onKeyUp = (e) => {
+        console.log("onKeyUp: ", e.keyCode);
+        for (let action_name in this.actions) {
+          let action = this.actions[action_name]
+          if(action.keys.indexOf( e.keyCode ) != -1 && action.enabled) {
+            action.active = false;
+          }
+        }
+        // }
+      }
+
     }
+
+    target.addEventListener( 'keydown', this.onKeyDown );
+    target.addEventListener( 'keyup', this.onKeyUp );
+    
   }
 
   detach() {
-    this.target = false;
-    //this.disableAction()
-    //this.target.removeEventListener('keypress', );
-    //this.target.removeEventListener('keypress', this.disableAction);
+    
+    if( !this.target ) return;
+
+    this.enabled = false;
+    this.target.removeEventListener( 'keydown', this.onKeyDown );
+    this.target.removeEventListener( 'keyup', this.onKeyUp );
+    this.target = undefined;
   }
 
   enableAction(action_name) {
-    
+    var action = this.actions[action_name];
+    if( !action ) return;
+    action.enabled = true;
   }
 
   disableAction(action_name) {
-    
+    var action = this.actions[action_name];
+    if( !action ) return;
+    action.enabled = false;
   }
 
-  isActionActive(action) {
-    if(this.actionsToBind[action].enabled) return false;
-    return true;
+  isActionActive(action_name) {
+    var action = this.actions[action_name];
+    if( !action ) return;
+    return action.active;
   }
 
   isKeyPressed(keyCode) {
-    
+    for (let action_name in this.actions) {
+      let action = this.actions[action_name]
+      if(action.keys.indexOf( keyCode ) != -1 && action.active) {
+        return true;
+      }
+    }
+    return false;
   }
 
 }
